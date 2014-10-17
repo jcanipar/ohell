e5080ff3954e3ea1b97d72e608bbb0eea10c5e1c
Jacob Caniparoli <jcanipar@gmail.com>
2014-10-16 23:45:05 -0700

    added rounds scaffold

--- ohell/config/routes.rb
+++ ohell/config/routes.rb
@@ -1,5 +1,11 @@
 Ohell::Application.routes.draw do
-  resources :games
+  resources :rounds
+
+  resources :games do
+    resources :rounds
+  end
+
+  root to: 'games#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
