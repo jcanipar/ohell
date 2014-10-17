582e94dc822f7ea10c2caead0109d92b43ac636c
Jacob Caniparoli <jcanipar@gmail.com>
2014-10-16 21:07:34 -0700

    Initial commit

---
+++ ohell/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ohell::Application.config.secret_key_base = 'b4d95ba0d149a6a0436a4a2bcb412f44d9968aa472b8d67a860ae56703d4793ba86cce2c743abdc0434236152ba23659fa3b7d12b0d8c79c748b4c9dd403ec24'
---
+++ ohell/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ohell::Application.config.session_store :cookie_store, key: '_ohell_session'
