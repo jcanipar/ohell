02a6fd3843d8da9ecc6c95714f91aa58e98974b8
Jacob Caniparoli <jcanipar@gmail.com>
2016-10-03 21:59:04 -0700

    Added admin and approved columns to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004023553) do
+ActiveRecord::Schema.define(version: 20161004044651) do
 
   create_table "boxes", force: true do |t|
     t.string   "name"
@@ -129,6 +129,8 @@ ActiveRecord::Schema.define(version: 20161004023553) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "approved"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
