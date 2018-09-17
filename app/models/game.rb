0480bf72aaf008b56ef958cb2e13715e7b6785b8
Jacob Caniparoli <jcanipar@gmail.com>
2018-09-16 17:46:51 -0700

    Upgraded to ruby 2.3.7

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
 
-   has_many :rounds, :order => 'place DESC'
+   has_many :rounds, -> { order 'place desc' }
+   #:order => 'place DESC'
 	accepts_nested_attributes_for :rounds
 
    	def winner_by_id
@@ -17,6 +18,7 @@ class Game < ActiveRecord::Base
 
    	def winner
    		r = self.rounds
+         puts r
    		r = r.sort_by{|e| -e[:score]}
    		return r.first.player
    	end
